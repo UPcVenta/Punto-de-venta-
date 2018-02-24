d51c9ed20161cee021ae9955ee78e77f2113087f
Usuario <Usuario@192.168.1.79>
2018-02-23 19:46:09 -0600

    Conexion...

--- src/puntoventa/PuntoVenta.java
+++ src/puntoventa/PuntoVenta.java
@@ -4,16 +4,25 @@
  * and open the template in the editor.
  */
 package puntoventa;
+import java.sql.*;
 
 /**
  *
  * @author Usuario
  */
 public class PuntoVenta {
-
-    /**
-     * @param args the command line arguments
-     */
+   
+   Connection conectar = null;
+   public Connection conexion(){
+       try{
+           Class.forName("com.mysql.jdbc.Driver");
+           conectar=DriverManager.getConnection("jdbc:mysql://localhost/test-db", "root", "");
+       }
+       catch(ClassNotFoundException | SQLException e) {
+           System.out.print(e.getMessage());
+       }
+       return conectar;
+   }
     public static void main(String[] args) {
         // TODO code application logic here
     }
